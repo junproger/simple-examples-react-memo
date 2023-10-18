5b27755441ef23e691d304f619e80c6ebd5697cc
Junior Programmer <progphyton@gmail.com>
2023-10-18 12:41:38 +0300

    feat: add uid/key helper

---
+++ source/helpers/uidkey.ts
@@ -0,0 +1,7 @@
+export const uid = (indx: number): string => {
+  const utf8 = 65;
+  const sign = '#';
+  const numb = indx + 1;
+  const char = utf8 + indx;
+  return sign + numb + String.fromCharCode(char);
+};
