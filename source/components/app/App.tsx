8964b07319dde6d94f5008d6f9e3d8556c3556ac
Junior Programmer <progphyton@gmail.com>
2023-10-18 13:21:56 +0300

    feat: add content and styles to app

--- source/components/app/App.tsx
+++ source/components/app/App.tsx
@@ -1,9 +1,18 @@
 import { FC } from 'react';
 
+import * as styles from './app.module.css';
+
 export const App: FC = () => {
   return (
     <>
-      <div>App Component</div>
+      <main className={styles['app']}>
+        <div>
+          <p>Example 1</p>
+        </div>
+        <div>
+          <p>Example 2</p>
+        </div>
+      </main>
     </>
   );
 };
