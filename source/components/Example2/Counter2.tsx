2286f6e564824c00883b7bc01a8223f1e1a9f9ba
Junior Programmer <progphyton@gmail.com>
2023-10-21 08:36:54 +0300

    feat: add list component to second example

--- source/components/Example2/Counter2.tsx
+++ source/components/Example2/Counter2.tsx
@@ -1,10 +1,9 @@
 import { FC, useCallback, useState } from 'react';
 
-import { uidkey } from '../../helpers/uidkey';
 import { logging } from '../../utils/logging';
 
-import { Item2 } from './Item2';
 import { Result2 } from './Result2';
+import { List2 } from './List2';
 
 export const Counter2: FC = () => {
   logging('COUNTER-2 is rendered');
@@ -18,13 +17,7 @@ export const Counter2: FC = () => {
     <>
       <Result2 counters={getCounters} values={getTotalSum} />
       <button onClick={addCounter}>Add More Items++</button>
-      <div>
-        {[...Array(getCounters).keys()].map((index) => (
-          <Item2 key={uidkey(index)} summator={summator} uid={uidkey(index)}>
-            Increase value
-          </Item2>
-        ))}
-      </div>
+      <List2 counters={getCounters} summator={summator} />
     </>
   );
 };
---
+++ source/components/Example2/List2.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+
+import { uidkey } from '../../helpers/uidkey';
+import { logging } from '../../utils/logging';
+
+import { Item2 } from './Item2';
+
+export interface List2Prop {
+  counters: number;
+  summator: () => void;
+}
+
+export const List2: FC<List2Prop> = ({ counters, summator }) => {
+  logging('LIST-2 is rendered');
+  return (
+    <div>
+      {[...Array(counters).keys()].map((index) => (
+        <Item2 key={uidkey(index)} summator={summator} uid={uidkey(index)}>
+          Increase value
+        </Item2>
+      ))}
+    </div>
+  );
+};
